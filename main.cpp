acf655684e9f8078627556579c428164e7a28f46
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-02-17 23:46:33 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,210 +1,254 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <string>
+#include <cmath>
 
 using namespace std;
 using namespace sf;
 
-//Init game const
-    const double SCREEN_WIDTH=800;
-    const double SCREEN_HEIGH=600;
-    const int borderLeft=140;
-    const int borderRight=660;
-    const int racerWidth=40;
-    const int racerHeight=70;
-    string stringscore="";
-    int score=0;
-    double gameSpeed=0.3;
+//Game constant
+    const double SCREEN_WIDTH = 1280;
+    const double SCREEN_HEIGHT = 720;
+    const int borderLeft = 140;
+    const int borderRight = 660;
 
 //Create Main Windows
-RenderWindow app(VideoMode(SCREEN_WIDTH, SCREEN_HEIGH), "Car Racing");
+RenderWindow app(VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT),"Game kakkak V.1");
 
-//Creat random Number for game loop
-int getRandomNumber(int a, int b);
+//Car status
+struct Car{
+    double x,y,lastspeed,speed,dist;
+    bool state;
+};
 
-// Create gameover screen
-int gameOver();
+//Game over
+int gameOver(int );
 
 int main()
 {
-    //Init game music
-    SoundBuffer gameSoundBuffer;
-        gameSoundBuffer.loadFromFile("sound/game.wav");
-    Sound GameSound;
-        GameSound.setBuffer(gameSoundBuffer);
+    //Game music
+    SoundBuffer gameSoundBuffer,clicking,hovering;
+    gameSoundBuffer.loadFromFile("sound/game.wav");
+    clicking.loadFromFile("sound/click.wav");
+    hovering.loadFromFile("sound/hover.wav");
+    Sound GameSound,click,hover;
+    GameSound.setBuffer(gameSoundBuffer);
+    click.setBuffer(clicking);
+    hover.setBuffer(hovering);
 
-    Texture background, racer, obs1, obs2, obs3, obs4,gameover;
-    //Init font
-    Font myfont;
-    myfont.loadFromFile("font/xirod.ttf");
+    //Include texture and font
+    Texture background, racer, button1, button2, ticon, vicon, cb1 ,cb2;
+    Font font;
 
     //Load all images
-    if (!background.loadFromFile("cars/background.png")) return EXIT_FAILURE;
-    if (!racer.loadFromFile("cars/racer.png")) return EXIT_FAILURE;
-    if (!obs1.loadFromFile("cars/obs1.png")) return EXIT_FAILURE;
-    if (!obs2.loadFromFile("cars/obs2.png")) return EXIT_FAILURE;
-    if (!obs3.loadFromFile("cars/obs3.png")) return EXIT_FAILURE;
-    if (!obs4.loadFromFile("cars/obs4.png")) return EXIT_FAILURE;
+    if(!background.loadFromFile("cars/background.png")) return 10;
+    if(!racer.loadFromFile("cars/racer.png")) return 11;
+    if(!font.loadFromFile("font/xirod.ttf")) return 12;
+    if(!button1.loadFromFile("ui/tr.png")) return 13;
+    if(!button2.loadFromFile("ui/vs.png")) return 14;
+    if(!cb1.loadFromFile("ui/tr2.png")) return 15;
+    if(!cb2.loadFromFile("ui/vs2.png")) return 16;
+    if(!ticon.loadFromFile("ui/clock.png")) return 17;
+    if(!vicon.loadFromFile("ui/match.png")) return 18;
 
-    //Create sprite
-    Sprite Background(background),Background1(background),Racer(racer),Obs1(obs1),Obs2(obs2),Obs3(obs3),Obs4(obs4),Gameover(gameover);
+    //Background
+    Sprite Background(background),Background1(background);
 
-    double RacerX, RacerY,Obs1X, Obs1Y,Obs2X, Obs2Y,Obs3X, Obs3Y,Obs4X, Obs4Y;
+    //Time record mode button
+    Sprite spr_ButtonTR(button1);
+    spr_ButtonTR.setPosition(((SCREEN_WIDTH/2)-180),250);
 
-    //Set racer and Obs pos
-	RacerX=SCREEN_WIDTH/2;
-	RacerY=SCREEN_HEIGH-racerHeight;
-	Obs1X=getRandomNumber(borderLeft,borderRight);
-	Obs2X=getRandomNumber(borderLeft,borderRight);
-	Obs3X=getRandomNumber(borderLeft,borderRight);
-	Obs4X=getRandomNumber(borderLeft,borderRight);
-    Obs1Y=0,Obs2Y=-100,Obs3Y=-200,Obs4Y=-300;
-    double BackgroundY1=0;
-    double BackgroundY2=-600;
+    //Vs mode button
+    Sprite spr_ButtonVS(button2);
+    spr_ButtonVS.setPosition(((SCREEN_WIDTH/2)-140),150);
 
-    GameSound.play();
-    GameSound.setLoop(true);
+    //Time record icon
+    Sprite spr_ticon(ticon);
+    spr_ticon.setPosition(1230,0);
+    spr_ticon.setScale(0.1,0.1);
+
+    //VS icon coming soon
+    Sprite spr_vicon(vicon);
+    spr_vicon.setPosition(750,500);
+
+    //Player car
+    Sprite playerCar(racer);
+    Car player = {((SCREEN_WIDTH/2)-290),SCREEN_HEIGHT/2,0,0,0,false};
+    double travel;
+
+    //Car position
+    double BackgroundY1 = 0;
+    double BackgroundY2 = -600;
+
+    //Modes
+    bool TRmode = 0;
+    bool VSmode = 0;
+    FloatRect clickableArea = spr_ButtonTR.getGlobalBounds();
+    FloatRect clickableArea2 = spr_ButtonVS.getGlobalBounds();
+
+    //Time
+    Clock clock,clock2;
+    Time dt;
+    float duration = 10.0f;
+
+    //Press P to start game text
+    Text text_MainWindowText ( "Press P To Start The Game", font);
+    text_MainWindowText.setCharacterSize( 15 );
+    text_MainWindowText.setColor( Color::White );
+    text_MainWindowText.setPosition( 450 , 30 );
 
     //GAME LOOP
-    while (app.isOpen())
-    {
-        //Init and count score
-        stringscore="SCORE:"+to_string(score);
-        Text text(stringscore, myfont, 15);
-        text.setPosition(5,0);
+    while(app.isOpen()){
 
-        //Set car position
-        Racer.setPosition(RacerX,RacerY);
-        Obs1.setPosition(Obs1X,Obs1Y);
-        Obs2.setPosition(Obs2X,Obs2Y);
-        Obs3.setPosition(Obs3X,Obs3Y);
-        Obs4.setPosition(Obs4X,Obs4Y);
-
-        //Creat scrolling background
-        Background.setPosition(0,BackgroundY1);
-        Background1.setPosition(0,BackgroundY2);
-        if (BackgroundY2>0)
-        {
-            BackgroundY1=0;
-            BackgroundY2=BackgroundY1-500;
-        }
-        BackgroundY1+=0.1;
-        BackgroundY2+=0.1;
-
-        //Set Obs LOOP
-        if (Obs1Y>SCREEN_HEIGH)
-            {Obs1Y=0;Obs1X=getRandomNumber(borderLeft,borderRight);score++;} else {Obs1Y=Obs1Y+gameSpeed;}
-        if (Obs2Y>SCREEN_HEIGH)
-            {Obs2Y=0;Obs2X=getRandomNumber(borderLeft,borderRight);score++;} else {Obs2Y=Obs2Y+gameSpeed;}
-        if (Obs3Y>SCREEN_HEIGH)
-            {Obs3Y=0;Obs3X=getRandomNumber(borderLeft,borderRight);score++;} else {Obs3Y=Obs3Y+gameSpeed;}
-        if (Obs4Y>SCREEN_HEIGH)
-            {Obs4Y=0;Obs4X=getRandomNumber(borderLeft,borderRight);score++;} else {Obs4Y=Obs4Y+gameSpeed;}
-
-        //Game level
-        if(score>10 && score<18) {gameSpeed=0.4;}
-        if(score>18 && score<25) {gameSpeed=0.5;}
-        if(score>25 && score<35) {gameSpeed=0.6;}
-
-        //Create event to handle input from keyboard
         Event event;
+        //Initial position
+        playerCar.setPosition(player.x,player.y);
+
         while (app.pollEvent(event))
         {
-            if (event.type == Event::Closed)
-                app.close();
-            if (event.type == sf::Event::KeyPressed)
-                {
-                    if (event.key.code == sf::Keyboard::Left)
-                        {if(RacerX>borderLeft) {RacerX=RacerX-10;}}
-                    if (event.key.code == sf::Keyboard::Right)
-                        {if(RacerX<borderRight){RacerX=RacerX+10;}}
-                    if (event.key.code == sf::Keyboard::Up)
-                        {if(RacerY>0){RacerY=RacerY-10;}}
-                    if (event.key.code == sf::Keyboard::Down)
-                        {if(RacerY<SCREEN_HEIGH-70){RacerY=RacerY+10;}}
-                }
+                switch(event.type){
+                    //In case of clicking exit
+                    case Event::Closed:
+                        app.close();
+                        break;
+
+                    //In case of keyboard press
+                    case Event::KeyPressed:
+                    //Press P to start
+                    if(Keyboard::isKeyPressed(Keyboard::P)){
+                        GameSound.setVolume(0.6);
+                        GameSound.play();
+                        GameSound.setLoop(true);
+                        clock.restart();
+                        while(duration > 0){
+                            //Time setting
+                            Time time = clock.getElapsedTime();
+                            duration -= time.asSeconds();
+                            if(TRmode == 0) duration = 10;
+                            cout << duration << endl;
+                            clock.restart();
+                            double dt = time.asSeconds();
+                            dt *= 100;
+
+                            //Set car position
+                            playerCar.setPosition(player.x,player.y);
+
+                            //Create scrolling background
+                            Background.setPosition(0,BackgroundY1);
+                            Background1.setPosition(0,BackgroundY2);
+                            if (BackgroundY2>0)
+                            {
+                                BackgroundY1=0;
+                                BackgroundY2=BackgroundY1-500;
+                            }
+                            BackgroundY1 += player.dist;
+                            BackgroundY2 += player.dist;
+
+                            //Press key car speed up
+                            if(player.state == 0){
+                                if(Keyboard::isKeyPressed(Keyboard::A)) {player.speed += 2;player.state = 1;}
+                            }
+                            else if(!Keyboard::isKeyPressed(Keyboard::A)) player.state = 0;
+
+                            //Car go
+                            player.lastspeed = player.speed;
+                            player.speed -= 0.1*dt ;
+                            if(player.speed <= 0 ) player.speed = 0;
+                            player.dist = 0.5*(player.lastspeed+player.speed)*dt;
+
+
+                            //Distance traveled
+                            travel += player.dist;
+                            Text text("Distance " + to_string((int)travel) + " km",font,15);
+                            text.setPosition(5,0);
+
+                            //Clear and redraw position
+                            app.clear();
+                            app.draw(Background);
+                            app.draw(Background1);
+                            app.draw(playerCar);
+                            if(TRmode == 1){
+                                app.draw(spr_ticon);
+                                Text timer("Time left : " + to_string(duration) + " seconds",font,15);
+                                timer.setPosition(880,0);
+                                app.draw(timer);
+                            }
+                            app.draw(text);
+                            app.display();
+                        }
+                    }
+                    break;
+
+                    //In case of mouse clicking
+                    case Event::MouseButtonPressed:
+                        if (event.mouseButton.button == Mouse::Left) {
+                            Vector2f mousePosition = app.mapPixelToCoords(Mouse::getPosition(app));
+                            if (clickableArea.contains(mousePosition)) {
+                                hover.play();
+                                if(spr_ButtonTR.getTexture() == &button1){
+                                    TRmode = 1 ;
+                                    spr_ButtonTR.setTexture(cb1);
+                                    spr_ButtonVS.setTexture(button2);
+                                }
+                                else{
+                                    TRmode = 0 ;
+                                    spr_ButtonTR.setTexture(button1);
+                                }
+                            }
+                            if (clickableArea2.contains(mousePosition)) {
+                                hover.play();
+                                if(spr_ButtonVS.getTexture() == &button2){
+                                    spr_ButtonVS.setTexture(cb2);
+                                    spr_ButtonTR.setTexture(button1);
+                                }
+                                else{
+                                    TRmode = 0 ;
+                                    spr_ButtonVS.setTexture(button2);
+                                }
+                            }
+                        }
+                        break;
+
+                    default:
+                        break;
+            }
         }
-
-        //Check if accident happen
-        if (((RacerX>=(Obs1X-30)) &&(RacerX<=(Obs1X+30)))&&((RacerY>=(Obs1Y-30)) &&(RacerY)<=(Obs1Y+30)))
-            {
-                GameSound.stop();gameOver();
-            };
-        if (((RacerX>=(Obs2X-30)) &&(RacerX<=(Obs2X+30)))&&((RacerY>=(Obs2Y-30)) &&(RacerY)<=(Obs2Y+30)))
-            {
-                GameSound.stop();gameOver();
-            };
-        if (((RacerX>=(Obs3X-30)) &&(RacerX<=(Obs3X+30)))&&((RacerY>=(Obs3Y-30)) &&(RacerY)<=(Obs3Y+30)))
-            {
-                GameSound.stop();gameOver();
-            };
-        if (((RacerX>=(Obs4X-30)) &&(RacerX<=(Obs4X+30)))&&((RacerY>=(Obs4Y-30)) &&(RacerY)<=(Obs4Y+30)))
-            {
-                GameSound.stop();gameOver();
-            };
-
-        //Clear and redraw position
+        //When mode is TR and time < 0 go to GameOver
+        if(TRmode == 1 and duration <=0){
+            sleep(seconds(2));
+            click.play();
+            gameOver(travel);
+        }
         app.clear();
+        app.draw(playerCar);
         app.draw(Background);
         app.draw(Background1);
-        app.draw(Racer);
-        app.draw(Obs1);
-        app.draw(Obs2);
-        app.draw(Obs3);
-        app.draw(Obs4);
-        app.draw(text);
+        app.draw(text_MainWindowText);
+        app.draw(spr_ButtonVS);
+        app.draw(spr_ButtonTR);
         app.display();
     }
-    return EXIT_SUCCESS;
+    return 0;
 }
-int startGame()
-{
-    //TODO
-}
-//Game over
-int gameOver()
-{
-    Texture gameover,troll;
-        if (!gameover.loadFromFile("cars/over.png")) return EXIT_FAILURE;
-        if(!troll.loadFromFile("cars/troll.png")) return EXIT_FAILURE;
-
+//Game over screen
+int gameOver(int dis){
+    Texture gameover;
+    Font font;
+    if(!gameover.loadFromFile("cars/over.png")) return 20;
+    if(!font.loadFromFile("font/xirod.ttf")) return 12;
     Sprite Gameover(gameover);
-    Sprite Troll(troll);
-    Troll.setPosition(10,350);
-    SoundBuffer gameOver;
-        gameOver.loadFromFile("sound/crash.wav");
-    Sound GameOver;
-        GameOver.setBuffer(gameOver);
-        GameOver.play();
-    while (app.isOpen())
-    {
-        Event event;
-        while (app.pollEvent(event))
-        {
-            if (event.type == Event::Closed)
-                app.close();
-        }
-        Font myfont;
-            myfont.loadFromFile("font/xirod.ttf");
-            stringscore="YOUR SCORE:"+to_string(score);
-        Text text(stringscore, myfont, 30);
-            text.setPosition(210,450);
+    while(app.isOpen()){
+        Text go("You Got " + to_string(dis) + " km",font,30);
+        go.setPosition(SCREEN_WIDTH/2-200,SCREEN_HEIGHT/2);
         app.clear();
-        app.draw(Gameover);
-        app.draw(text);
-        app.draw(Troll);
+        app.draw(go);
         app.display();
-
+        sleep(seconds(3));
+        app.close();
     }
-
+    return 999;
 }
-int getRandomNumber(int a, int b)
-    {
-        static bool first = true; if (first){srand( time(NULL) );first = false;}
-        int result=a + rand() % (( b + 1 ) - a);
-        result=(result/10)*10;
-        return result;
-    }
+
